99e65444f3f998c30e5218afdb9befa7c62ec44e
felipevilla2105-ops <felipe.villa2105@gmail.com>
2025-10-01 08:19:16 -0500

    avance0110

--- app2.py
+++ app2.py
@@ -5,6 +5,7 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 import os
 
+
 def clasificar(file): 
     # Leer el archivo
     df = pd.read_json(file.name)
